6b972ec02c1b4853b34154033abfc5a0ab0b9157
Youngmok Cho <choym0098@gmail.com>
2024-03-09 19:18:55 -0800

    Add option to delete command

--- nurc/src/command/delete.rs
+++ nurc/src/command/delete.rs
@@ -2,6 +2,7 @@ use clap::Args;
 
 #[derive(Args)]
 pub struct DeleteArgs {
+    /// Forcibly delete the running container, using SIGKILL signal(7) to stop it first.
     #[arg(short, long)]
     force: bool,
 }
